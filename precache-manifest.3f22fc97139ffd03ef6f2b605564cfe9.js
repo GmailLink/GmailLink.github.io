772c8dc3d3147ee935490ddc7918e221630eb462
sarat <trickstop99@gmail.com>
2020-05-10 21:28:44 +0530

    Updates

---
+++ precache-manifest.3f22fc97139ffd03ef6f2b605564cfe9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c35679a756be9d83dbd08bce39c55c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd4e1ddf23b0537171a2",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2b8c6a62cfbb48a6b778",
+    "url": "/static/css/main.95ee6e40.chunk.css"
+  },
+  {
+    "revision": "bd4e1ddf23b0537171a2",
+    "url": "/static/js/2.68e09902.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.68e09902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8c6a62cfbb48a6b778",
+    "url": "/static/js/main.6ec95e99.chunk.js"
+  },
+  {
+    "revision": "24678e76f5abe8bd3ef6",
+    "url": "/static/js/runtime-main.e9c97ce4.js"
+  },
+  {
+    "revision": "82d5fafd474b495369a36372191a387c",
+    "url": "/static/media/gmailLogo.82d5fafd.ico"
+  }
+]);
