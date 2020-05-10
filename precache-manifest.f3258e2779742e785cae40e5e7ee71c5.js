b00fd64b1d5dc9f4bc7e904f2453c476f7742bb0
sarat <trickstop99@gmail.com>
2020-05-10 23:07:43 +0530

    Updates

---
+++ precache-manifest.f3258e2779742e785cae40e5e7ee71c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754ace956d830b4f9470058dcdb4f761",
+    "url": "/glink/index.html"
+  },
+  {
+    "revision": "a37d7340cea7f1cbb335",
+    "url": "/glink/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d78932b7c6904c17dd77",
+    "url": "/glink/static/css/main.95ee6e40.chunk.css"
+  },
+  {
+    "revision": "a37d7340cea7f1cbb335",
+    "url": "/glink/static/js/2.dcdd6b4c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/glink/static/js/2.dcdd6b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78932b7c6904c17dd77",
+    "url": "/glink/static/js/main.89c99e16.chunk.js"
+  },
+  {
+    "revision": "bad4af0a84374bffd388",
+    "url": "/glink/static/js/runtime-main.c7c44a1e.js"
+  },
+  {
+    "revision": "82d5fafd474b495369a36372191a387c",
+    "url": "/glink/static/media/gmailLogo.82d5fafd.ico"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50502d5e6314d755191ceb103da9196d.js"
+  "/glink/precache-manifest.f3258e2779742e785cae40e5e7ee71c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/glink/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
