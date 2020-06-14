fa243df4ee97cbd0b43f0b02a73c67420ee12702
SarathTracxn <sarathbabu.v@tracxn.com>
2020-06-14 14:55:13 +0530

    Bugs fix

---
+++ precache-manifest.094a834e120bf65e9b92bce4d29cc727.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb8b51e74d53074080a0133d4f2348d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad9b424d1d1abc2b201",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "539f03b3bf0be9ac8ee3",
+    "url": "/static/css/main.f786fb3c.chunk.css"
+  },
+  {
+    "revision": "aad9b424d1d1abc2b201",
+    "url": "/static/js/2.91ab9c4f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.91ab9c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539f03b3bf0be9ac8ee3",
+    "url": "/static/js/main.7919531b.chunk.js"
+  },
+  {
+    "revision": "52dd8c8f4d71c598837c",
+    "url": "/static/js/runtime-main.f3780103.js"
+  },
+  {
+    "revision": "82d5fafd474b495369a36372191a387c",
+    "url": "/static/media/gmailLogo.82d5fafd.ico"
+  },
+  {
+    "revision": "c02edc1f7d2c7381d622f1528604c010",
+    "url": "/static/media/suggestions.c02edc1f.svg"
+  }
+]);
